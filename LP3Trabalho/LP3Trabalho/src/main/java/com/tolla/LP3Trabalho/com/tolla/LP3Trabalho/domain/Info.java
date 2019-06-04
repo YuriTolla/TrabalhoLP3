5de437cba1aa1cfe8c48644fbd70f99390505ef3
Yuri Rodrigues Tolla da Silva <yuritolla@gmail.com>
2019-06-03 21:46:35 -0300

    Add files via upload

---
+++ LP3Trabalho/LP3Trabalho/src/main/java/com/tolla/LP3Trabalho/com/tolla/LP3Trabalho/domain/Info.java
@@ -0,0 +1,13 @@
+package com.tolla.LP3Trabalho.com.tolla.LP3Trabalho.domain;
+
+public class Info {
+    private Tempo tempo;
+
+    public Tempo getTempo() {
+        return tempo;
+    }
+
+    public void setTempo(Tempo tempo) {
+        this.tempo = tempo;
+    }
+}
